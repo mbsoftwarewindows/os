23fb0b485ed6d10a39d1abf881bf81b2db55f841
mbsoftwarewindows <mbsoftwarewindows@gmail.com>
2016-11-26 20:48:44 -0500

    Change codename to "ion"
    Change codename to “ion”

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -107,5 +107,5 @@ void kernel_main(void) {
     terminal_initialize();
     
     /* Newline support is left as an exercise. */
-    terminal_writestring("Welcome To OS!\n");
+    terminal_writestring("Welcome To ION!\n");
 }
